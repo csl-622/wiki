5c1649bfa0a83e2e1ff8d350bcbf383b40be1da2
Prinshu Kumar <2016csb1051@iitrpr.ac.in>
2018-11-13 21:26:32 +0530

    Update test.py

--- Application/test.py
+++ Application/test.py
@@ -9,18 +9,18 @@ class application(QtGui.QMainWindow):
 		QtGui.QMainWindow.__init__(self)
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
-        self.showMaximized()
-        self.ui.search.clicked.connect(lambda: self.findArticle(self.ui.topic.text()))
-        r = self.app.exec_()
-        sys.exit(r)
+        	self.showMaximized()
+        	self.ui.search.clicked.connect(lambda: self.findArticle(self.ui.topic.text()))
+        	r = self.app.exec_()
+        	sys.exit(r)
         	
 	def findArticle(self,topic):
 		if len(topic) == 0:
 			msgBox = QtGui.QMessageBox()
-        	msgBox.setText("Please enter a valid topic.")
-        	msgBox.setStandardButtons(QtGui.QMessageBox.Ok)
-        	msgBox.setDefaultButton(QtGui.QMessageBox.Ok)
-        	ret = msgBox.exec_()
+        		msgBox.setText("Please enter a valid topic.")
+        		msgBox.setStandardButtons(QtGui.QMessageBox.Ok)
+        		msgBox.setDefaultButton(QtGui.QMessageBox.Ok)
+        		ret = msgBox.exec_()
 		else:
 			print topic       	
 application()
